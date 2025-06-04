227c67c1030ba6b8724863a85311998b239ae7fe
Riley Pearce <riley@roguesalad.co>
2025-06-04 09:51:26 -0600

    Updating to have 2 versions of the logo for light and dark devices

--- components/BrandLogo.tsx
+++ components/BrandLogo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, StyleSheet, View, ImageStyle } from "react-native";
+import { Image, StyleSheet, View, ImageStyle, useColorScheme } from "react-native";
 
 type BrandLogoProps = {
   width?: number;
@@ -7,18 +7,23 @@ type BrandLogoProps = {
   style?: ImageStyle;
 };
 
-const LOGO_URI =
+const LOGO_DARK =
   "https://www.juncoshollinger.com/wp-content/themes/juncoshollinger/assets/images/logo.png";
+const LOGO_LIGHT =
+  "https://upload.wikimedia.org/wikipedia/en/9/9b/2023_Juncos_Hollinger_Racing_Logo.png";
 
 export default function BrandLogo({
   width = 250,
   height = 120,
   style,
 }: BrandLogoProps) {
+  const colorScheme = useColorScheme();
+  const logoUri = colorScheme === "light" ? LOGO_LIGHT : LOGO_DARK;
+
   return (
     <View style={styles.container}>
       <Image
-        source={{ uri: LOGO_URI }}
+        source={{ uri: logoUri }}
         style={[styles.logo, { width, height }, style]}
         resizeMode="contain"
       />
